208496138421b531901c032bed40a09ffc42c56a
Jeremy <jeremy_perthuis@outlook.com>
2019-11-16 18:11:06 +0100

    maj : Refonte BuildCover ok

---
+++ BuildCover.py
@@ -0,0 +1,90 @@
+import logging
+from pyfiglet import Figlet
+from math import *
+from Mp3Processing import *
+
+class BuildCover:
+
+    list_font = ["straight", "stop", "starwars", "standard", "small", "roman", "puffy", "doom", "big"]
+    default_font = "standard"
+    title_limit = 52
+    template_details = []
+    cover_export = []
+    logging.basicConfig(format='%(asctime)s  %(levelname)s : %(funcName)s  %(message)s')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+
+    def __init__(self, mp3Processing):
+        self.defaut_CD_title = mp3Processing.getDefautCDName()
+        self.complete_path = mp3Processing.music_folder_path
+        self.mp3_dict = mp3Processing.list_mp3
+        self.buildHeader()
+        self.buildCover()
+        self.displayCover()
+        self.writeTemplate()
+
+    # Construit le Header titre du tableau imprimable dans coverExport
+    def buildHeader(self):
+        logging.info("start")
+        lenTitle = 69
+        f = Figlet(font=self.default_font)
+        bigtext = str(f.renderText(self.defaut_CD_title))
+        bigtextFrag = bigtext.split("\n")
+        CdTitleCentre = floor(lenTitle / 2) - floor(len(bigtextFrag[0]) / 2)
+
+        # gere le decalage du titre
+        if len(bigtextFrag[0]) % 2 == 1:
+            ecart = 0
+        else:
+            ecart = 1
+        self.cover_export.append('╔' + (lenTitle) * '═' + '╗')
+        for ligne in bigtextFrag:
+            if len(ligne) > 0:
+                self.cover_export.append('║' + (CdTitleCentre) * ' ' + ligne + (CdTitleCentre + ecart) * ' ' + '║')
+        self.cover_export.append("╠══╦" + (self.title_limit) * '═' + '╦═════╦═══╦═══╣')
+
+    def displayCover(self):
+        for line in self.cover_export:
+            print(line)
+    # Construit le tableau avec les titres
+    def buildCover(self):
+        logging.info("start")
+        self.cover_export = []
+        self.buildHeader()
+        i = 0
+        for key, value in self.mp3_dict.items():
+            i += 1
+
+            # Formate le numero (1ere colonne)
+            arg0 = lambda x: str(x) if len(str(x)) >= 2 else '0' + str(x)
+            # Formate le tempo et la tonalité
+            arg34 = lambda x: ' ' + x if len(x) == 2 else x
+
+            self.cover_export.append('║{0}║{1}║{2}║{3}║{4}║'.format(
+                arg0(i),
+                value["artist"] + ' - '+ value["title"] + (self.title_limit - len(value["artist"] + ' - '+ value["title"])) * ' ',
+                value["length"],
+                arg34(value["key"]),
+                arg34(value["tempo"])))
+
+            if i < len(self.mp3_dict):
+                self.cover_export.append('╠══╬' + (self.title_limit) * '═' + '╬═════╬═══╬═══╣')
+
+            if i >= len(self.mp3_dict):
+                self.cover_export.append('╚══╩' + (self.title_limit) * '═' + '╩═════╩═══╩═══╝')
+
+
+    # Ecrit dans un fichier txt le cover final dans le dossier d'origine
+    def writeTemplate(self):
+        logging.info("start")
+        file = open(self.complete_path + "\\tracklist.txt", 'w', encoding="utf8")
+        for elem in self.cover_export:
+            file.write(elem + '\n')
+        file.close()
+        print("     ok")
+
+
+if __name__ == '__main__':
+    M = Mp3Processing("F:\\Users\\Jeremy\\Developpement\\PrintableMusicCoverGenerator\\testCD")
+    B = BuildCover(M)
--- Mp3Processing.py
+++ Mp3Processing.py
@@ -5,25 +5,19 @@
 '''
 
 import os
-from math import *
+
 import mutagen
 import datetime
 from Functions import *
-from pyfiglet import Figlet
+
 import logging
 
 class Mp3Processing:
     version = 1.5
     music_folder_path = ""
-    cover_titre = ""
-    title_limit = 52
-    list_font = ["straight", "stop", "starwars", "standard", "small", "roman", "puffy", "doom", "big"]
-    default_font = "standard"
-    list_music_title = []
-    list_music_title_format = []
+
     list_mp3 = dict()
-    template_details = []
-    cover_export = []
+
     logging.basicConfig(format='%(asctime)s  %(levelname)s : %(funcName)s  %(message)s')
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -35,13 +29,10 @@ class Mp3Processing:
         self.scanFolder()
         self.addlength()
         self.addTitleArtist()
-        # self.verification()
-        # self.formatListeMusic(self.list_music_title_format)
 
     # Stocke les titres dans l'ordre alphabetique dans listMusicTitle
     def scanFolder(self):
         logging.info("start")
-        self.list_music_title.clear()
         for file in os.listdir(self.music_folder_path):
             try:
                 mutagen.File(self.music_folder_path + "\\" + file)
@@ -52,6 +43,12 @@ class Mp3Processing:
             except mutagen.MutagenError:
                 logging.warning(" {0} n'est pas un fichier compatible".format(file))
         logging.info("end : {0} fichiers .mp3 traité(s)".format(len(self.list_mp3)))
+
+    # ajoute le noeud terminal du path (Nom pas défaut du CD)
+    def getDefautCDName(self):
+        logging.info("start")
+        return self.music_folder_path.split("\\")[-1]
+
 
     # Stocke les durée dans list_mp3
     def addlength(self):
@@ -73,65 +70,6 @@ class Mp3Processing:
             value["title"] = datas_dict["title"]
             value["key"] = datas_dict["key"]
             value["tempo"] = datas_dict["tempo"]
-
-    # Construit le Header titre du tableau imprimable dans coverExport
-    def buildHeader(self):
-        logging.info("start")
-        lenTitle = 69
-        f = Figlet(font=self.default_font)
-        bigtext = str(f.renderText(self.cover_titre))
-        bigtextFrag = bigtext.split("\n")
-        CdTitleCentre = floor(lenTitle / 2) - floor(len(bigtextFrag[0]) / 2)
-
-        # gere le decalage du titre
-        if len(bigtextFrag[0]) % 2 == 1:
-            ecart = 0
-        else:
-            ecart = 1
-        self.cover_export.append('╔' + (lenTitle) * '═' + '╗')
-        for ligne in bigtextFrag:
-            if len(ligne) > 0:
-                self.cover_export.append('║' + (CdTitleCentre) * ' ' + ligne + (CdTitleCentre + ecart) * ' ' + '║')
-        self.cover_export.append("╠══╦" + (self.title_limit) * '═' + '╦═════╦═══╦═══╣')
-
-    # Construit le tableau avec les titres
-    def buildCover(self):
-        logging.info("start")
-        self.cover_export = []
-        self.buildHeader()
-        i = 0
-        for elem in self.list_music_title_format:
-            i += 1
-            if len(elem) == 4:
-                # Formate le numero (1ere colonne)
-                arg0 = lambda x: str(x) if len(str(x)) >= 2 else '0' + str(x)
-                # Formate le tempo et la tonalité
-                arg34 = lambda x: ' ' + x if len(x) == 2 else x
-
-                self.cover_export.append('║{0}║{1}║{2}║{3}║{4}║'.format(
-                    arg0(i),
-                    elem[0] + (self.title_limit - len(elem[0])) * ' ',
-                    elem[3],
-                    arg34(elem[1]),
-                    arg34(elem[2])))
-
-            else:
-                print("{0} est mal formaté".format(elem))
-
-            if i < len(self.list_music_title_format):
-                self.cover_export.append('╠══╬' + (self.title_limit) * '═' + '╬═════╬═══╬═══╣')
-            if i >= len(self.list_music_title_format):
-                self.cover_export.append('╚══╩' + (self.title_limit) * '═' + '╩═════╩═══╩═══╝')
-        print("     buildCover() ok")
-
-    # Ecrit dans un fichier txt le cover final dans le dossier d'origine
-    def writeTemplate(self):
-        logging.info("start")
-        file = open(self.music_folder_path + "\\tracklist.txt", 'w', encoding="utf8")
-        for elem in self.cover_export:
-            file.write(elem + '\n')
-        file.close()
-        print("     ok")
 
 if __name__ == '__main__':
     M=Mp3Processing("F:\\Users\\Jeremy\\Developpement\\PrintableMusicCoverGenerator\\testCD")
